42eb8baaae895b66dcf3b5b5bc6a71f69c1945c6
Benjamin <Benjamin@DESKTOP-UAN0GNM>
2020-12-22 15:44:11 +0100

    [CursUTT] Init commit

---
+++ app/src/main/java/com/example/cursutt/ViewModels/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.cursutt.ViewModels;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.widget.Button;
+
+import com.example.cursutt.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button addButton;
+    RecyclerView recyclerView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        addButton = (Button) findViewById(R.id.main_addButton);
+        recyclerView = (RecyclerView) findViewById(R.id.main_recyclerView);
+
+        addButton.setOnClickListener(v -> createModule());
+    }
+
+    private void createModule(){
+        
+    }
+}
